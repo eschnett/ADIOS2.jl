09f15e8a15b1a4ca28afdd5fb452fed3353478dc
Erik Schnetter <schnetter@gmail.com>
2025-06-17 09:34:19 -0400

    CI: Debug CompatLowerBounds

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,26 +4,26 @@ using MPI
 using Printf
 using Test
 
-################################################################################
-# Find ADIOS2 version
-
-# There is no official way to find the ADIOS2 library version. Instead
-# we check the default engine type after opening a file.
-const ADIOS2_VERSION = let
-    adios = adios_init_serial()
-    io = declare_io(adios, "IO")
-    filename = tempname()
-    engine = open(io, filename, mode_write)
-    etype = type(engine)
-    close(engine)
-    if etype == "BP4Writer"
-        v"2.8.0"
-    elseif etype == "BP5Writer"
-        v"2.9.0"
-    else
-        @assert false
-    end
-end
+#TODO ################################################################################
+#TODO # Find ADIOS2 version
+#TODO 
+#TODO # There is no official way to find the ADIOS2 library version. Instead
+#TODO # we check the default engine type after opening a file.
+#TODO const ADIOS2_VERSION = let
+#TODO     adios = adios_init_serial()
+#TODO     io = declare_io(adios, "IO")
+#TODO     filename = tempname()
+#TODO     engine = open(io, filename, mode_write)
+#TODO     etype = type(engine)
+#TODO     close(engine)
+#TODO     if etype == "BP4Writer"
+#TODO         v"2.8.0"
+#TODO     elseif etype == "BP5Writer"
+#TODO         v"2.9.0"
+#TODO     else
+#TODO         @assert false
+#TODO     end
+#TODO end
 
 ################################################################################
 
