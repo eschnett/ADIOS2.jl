49c5fcf3ff588de6109dbc79a624cd39aa65d50a
Erik Schnetter <schnetter@gmail.com>
2025-06-17 09:51:34 -0400

    CI: No const

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,17 +29,17 @@ using Test
 
 # Initialize MPI
 let
-    const mpi_initialized = MPI.Initialized()
+    #=const=# mpi_initialized = MPI.Initialized()
     if !mpi_initialized
         println("Initializing MPI")
         MPI.Init()
     end
-    const comm = MPI.COMM_WORLD
-    const comm_rank = MPI.Comm_rank(comm)
-    const comm_size = MPI.Comm_size(comm)
+    #=const=# comm = MPI.COMM_WORLD
+    #=const=# comm_rank = MPI.Comm_rank(comm)
+    #=const=# comm_size = MPI.Comm_size(comm)
     println("This is MPI process $comm_rank/$comm_size")
-    const comm_root = 0
-    const use_mpi = comm_size > 1
+    #=const=# comm_root = 0
+    #=const=# use_mpi = comm_size > 1
     println("$(use_mpi ? "Enabling" : "Disabling") MPI tests")
 end
 
@@ -66,7 +66,7 @@ end
 
 # Finalize MPI
 let
-    const mpi_finalized = MPI.Finalized()
+    #=const=# mpi_finalized = MPI.Finalized()
     if mpi_initialized && !mpi_finalized
         println("Finalizing MPI")
         MPI.Finalize()
